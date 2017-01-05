3efefc69df23582be43cd9379d7e867409b6b5fd
mscharm <lokzock@gmail.com>
2017-01-05 22:23:30 +0800

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Huginn
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = ENV['TIMEZONE'].presence || "UCT+8 (Asia/Taipei)"
+    config.time_zone = ENV['TIMEZONE'].presence || "China Standard Time (Taipei)"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
